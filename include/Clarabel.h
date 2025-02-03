096df5ee32da24f60c5e6ca0c921e2f31f9ae366
Joris Gillis <joris.gillis42@gmail.com>
2025-02-03 12:43:30 +0100

    Harden c header for inclusion in c++

--- include/Clarabel.h
+++ include/Clarabel.h
@@ -1,11 +1,19 @@
 #ifndef CLARABEL_H
 #define CLARABEL_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "c/CscMatrix.h"
 #include "c/DefaultInfo.h"
 #include "c/DefaultSettings.h"
 #include "c/DefaultSolution.h"
 #include "c/DefaultSolver.h"
 #include "c/SupportedConeT.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // CLARABEL_H
